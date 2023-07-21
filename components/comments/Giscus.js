b2f00cc92d53156b27df7a30903051b0a3a621fa
Flik Jeong <jsh852@gmail.com>
2023-07-21 09:44:45 +0900

    Add giscus setup

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -32,16 +32,18 @@ const Giscus = () => {
 
     const script = document.createElement('script')
     script.src = 'https://giscus.app/client.js'
-    script.setAttribute('data-repo', repo)
-    script.setAttribute('data-repo-id', repositoryId)
-    script.setAttribute('data-category', category)
-    script.setAttribute('data-category-id', categoryId)
-    script.setAttribute('data-mapping', mapping)
-    script.setAttribute('data-reactions-enabled', reactions)
-    script.setAttribute('data-emit-metadata', metadata)
-    script.setAttribute('data-input-position', inputPosition)
-    script.setAttribute('data-lang', lang)
-    script.setAttribute('data-theme', commentsTheme)
+    script.setAttribute('data-repo', 'fliklab/tailwind-nextjs-blog')
+    script.setAttribute('data-repo-id', 'R_kgDOJ98S4A')
+    script.setAttribute('data-category', 'Comments')
+    script.setAttribute('data-category-id', 'DIC_kwDOJ98S4M4CYByC')
+    script.setAttribute('data-mapping', 'pathname')
+    script.setAttribute('data-strict', 1)
+    script.setAttribute('data-reactions-enabled', 1)
+    script.setAttribute('data-emit-metadata', 0)
+    script.setAttribute('data-input-position', 'top')
+    script.setAttribute('data-theme', 'light_tritanopia')
+    script.setAttribute('data-lang', 'ko')
+    script.setAttribute('data-loading', 'lazy')
     script.setAttribute('crossorigin', 'anonymous')
     script.async = true
 
@@ -62,9 +64,9 @@ const Giscus = () => {
   }, [LoadComments])
 
   return (
-    <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300">
+    <div className='pt-6 pb-6 text-center text-gray-700 dark:text-gray-300'>
       {enableLoadComments && <button onClick={LoadComments}>Load Comments</button>}
-      <div className="giscus" id={COMMENTS_ID} />
+      <div className='giscus' id={COMMENTS_ID} />
     </div>
   )
 }
