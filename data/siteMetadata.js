e2422740c76d5a7ed5c77a298266c72de0edd47a
Flik Jeong <jsh852@gmail.com>
2023-07-21 09:55:11 +0900

    Fix giscus id

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: '',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
